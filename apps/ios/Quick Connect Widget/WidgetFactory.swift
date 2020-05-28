739e480d9b36c7814fd8f031b6590a02bd76d1b4
Marc Flores <mrk.flores@protonmail.com>
2020-05-28 15:45:13 +0200

    Keep QC Widget simple [VPNAPPL-238]

--- apps/ios/Quick Connect Widget/WidgetFactory.swift
+++ apps/ios/Quick Connect Widget/WidgetFactory.swift
@@ -24,66 +24,39 @@ import Foundation
 import vpncore
 
 class AlertServiceStub: CoreAlertService {
-
-    func push(alert: SystemAlert) {
-    }
-    
+    func push(alert: SystemAlert) { }
 }
 
 class WidgetFactory {
     
     static var shared = WidgetFactory()
     
-    private let alamofireWrapper: AlamofireWrapper
-    private let vpnApiService: VpnApiService
-    private let vpnManager: VpnManager
-    private let vpnKeychain: VpnKeychainProtocol
-    private let configurationPreparer: VpnManagerConfigurationPreparer
-    
-    private var _vpnGateway: VpnGatewayProtocol?
-    
-    var vpnGateway: VpnGatewayProtocol? {
-        guard let _ = try? vpnKeychain.fetch() else {
-            _vpnGateway = nil
-            return nil
-        }
-        
-        if _vpnGateway == nil {
-            _vpnGateway = VpnGateway(vpnApiService: vpnApiService, appStateManager: appStateManager, alertService: alertService, vpnKeychain: vpnKeychain, siriHelper: SiriHelper())
-        }
-        
-        return _vpnGateway
-    }
-    
+    private let openVpnExtensionBundleIdentifier = "ch.protonmail.vpn.OpenVPN-Extension"
+    private let appGroup = "group.ch.protonmail.vpn"
+
     let alertService = ExtensionAlertService()
-    
     let propertiesManager = PropertiesManager()
-    
-    lazy var appStateManager = { [unowned self] in
-        return AppStateManager(vpnApiService: vpnApiService, vpnManager: vpnManager, alamofireWrapper: alamofireWrapper, alertService: alertService, timerFactory: TimerFactory(), propertiesManager: propertiesManager, vpnKeychain: vpnKeychain, configurationPreparer: configurationPreparer)
-    }()
-    
+
     var todayViewModel:TodayViewModel {
-        let viewModel = TodayViewModelImplementation( self )
+        let viewModel = TodayViewModelImplementation( self.propertiesManager, vpnManager: self.vpnManager )
         self.alertService.delegate = viewModel
         return viewModel
     }
     
     private init() {
         setUpNSCoding(withModuleName: "ProtonVPN")
-        Storage.setSpecificDefaults(defaults: UserDefaults(suiteName: "group.ch.protonmail.vpn")!)
+        Storage.setSpecificDefaults(defaults: UserDefaults(suiteName: self.appGroup)!)
+    }
+    
+    // MARK: - Lazy
+    
+    lazy var vpnManager: VpnManagerProtocol = {
+        let openVpnFactory = OpenVpnProtocolFactory(bundleId: self.openVpnExtensionBundleIdentifier,
+                                                    appGroup: self.appGroup,
+                                                    propertiesManager: self.propertiesManager)
         
-        alamofireWrapper = AlamofireWrapperImplementation()
-        vpnApiService = VpnApiService(alamofireWrapper: alamofireWrapper)
-        let openVpnExtensionBundleIdentifier = "ch.protonmail.vpn.OpenVPN-Extension"
-        let appGroup = "group.ch.protonmail.vpn"
-        vpnManager = VpnManager(ikeFactory: IkeProtocolFactory(), openVpnFactory: OpenVpnProtocolFactory(bundleId: openVpnExtensionBundleIdentifier, appGroup: appGroup, propertiesManager: propertiesManager), appGroup: appGroup)
-        vpnKeychain = VpnKeychain()
-        configurationPreparer = VpnManagerConfigurationPreparer(vpnKeychain: vpnKeychain, alertService: alertService)
-    }
-    
-    public func refreshVpnManager() {
-        vpnManager.refreshManagers()
-    }
-    
+        return VpnManager(ikeFactory: IkeProtocolFactory(),
+                          openVpnFactory: openVpnFactory,
+                          appGroup: self.appGroup)
+    }()
 }
--- apps/ios/Quick Connect Widget/viewModels/TodayViewModel.swift
+++ apps/ios/Quick Connect Widget/viewModels/TodayViewModel.swift
@@ -34,31 +34,25 @@ class TodayViewModelImplementation: TodayViewModel {
     weak var viewController: TodayViewControllerProtocol?
     
     private let reachability = Reachability()
-    private let factory: WidgetFactory
-//    private var vpnGateway: VpnGatewayProtocol?
+    private var timer: Timer?
+    private let propertiesManager: PropertiesManager
+    private let vpnManager: VpnManagerProtocol
     
-    init( _ factory: WidgetFactory ){
-        self.factory = factory
+    init( _ propertiesManager: PropertiesManager, vpnManager: VpnManagerProtocol ){
+        self.propertiesManager = propertiesManager
+        self.vpnManager = vpnManager
     }
     
     func viewDidLoad() {
-        factory.refreshVpnManager()
-//        vpnGateway = factory.vpnGateway
-        
-//        guard vpnGateway != nil else {
-//            viewController?.displayNoGateWay()
-//            return
-//        }
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(connectionChanged), name: VpnGateway.connectionChanged, object: nil)
         reachability?.whenReachable = { [weak self] _ in self?.connectionChanged() }
         reachability?.whenUnreachable = { [weak self] _ in self?.viewController?.displayUnreachable() }
         try? reachability?.startNotifier()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { [weak self] _ in
+            self?.connectionChanged()
+        })
     }
     
     func viewWillAppear(_ animated: Bool) {
-        ProfileManager.shared.refreshProfiles()
-        factory.refreshVpnManager()
         connectionChanged()
     }
     
@@ -66,10 +60,9 @@ class TodayViewModelImplementation: TodayViewModel {
     
     // MARK: - Utils
     
-    private func displayConnection() {
-        let properties = factory.propertiesManager
-        guard let request = properties.lastConnectionRequest,
-            let connection = request.vpnProtocol == .ike ? properties.lastIkeConnection : properties.lastOpenVpnConnection else {
+    private func displayConnected() {
+        guard let request = propertiesManager.lastConnectionRequest,
+            let connection = request.vpnProtocol == .ike ? propertiesManager.lastIkeConnection : propertiesManager.lastOpenVpnConnection else {
                 viewController?.displayNoGateWay()
                 return
         }
@@ -86,52 +79,46 @@ class TodayViewModelImplementation: TodayViewModel {
             viewController?.displayUnreachable()
             return
         }
-         
         
-
+        guard propertiesManager.hasConnected else {
+            viewController?.displayNoGateWay()
+            return
+        }
         
-//        viewController?.displayNoGateWay()
-        displayConnection()
-//        guard let vpnGateway = vpnGateway else {
-//            viewController?.displayNoGateWay()
-//            return
-//        }
-//
-//        switch vpnGateway.connection {
-//        case .connected:
-//            let server = appStateManager.activeConnection()?.server
-//            let country = LocalizationUtility.default.countryName(forCode: server?.countryCode ?? "")
-//            let ip = server?.ips.first?.entryIp
-//            viewController?.displayConnected(ip, country: country)
-//
-//        case .connecting:
-//            viewController?.displayConnecting()
-//
-//        case .disconnected, .disconnecting:
-//            viewController?.displayDisconnected()
-//        }
+        vpnManager.refreshManagers()
+        vpnManager.refreshState()
+        
+        switch vpnManager.state {
+        case .connected:
+            displayConnected()
+        case .connecting:
+            viewController?.displayConnecting()
+        case .disconnected, .disconnecting:
+            viewController?.displayDisconnected()
+        default:
+            viewController?.displayDisconnected()
+            break
+        }
     }
     
     @objc func connectAction(_ sender: Any) {
+        guard propertiesManager.hasConnected else {
+            if let url = URL(string: URLConstants.deepLinkBaseUrl) {
+                viewController?.extensionOpenUrl(url)
+            }
+            return
+        }
         
-//        guard let vpnGateway = vpnGateway else {
-//            // not logged in so open the app or the connection failed
-//            if let url = URL(string: URLConstants.deepLinkBaseUrl) {
-//                viewController?.extensionOpenUrl(url)
-//            }
-//            return
-//        }
-//
-//        switch vpnGateway.connection {
-//        case .connected, .connecting:
-//            if let url = URL(string: URLConstants.deepLinkDisconnectUrl) {
-//                viewController?.extensionOpenUrl(url)
-//            }
-//        case .disconnected, .disconnecting:
-//            if let url = URL(string: URLConstants.deepLinkConnectUrl) {
-//                viewController?.extensionOpenUrl(url)
-//            }
-//        }
+        switch vpnManager.state {
+        case .connected, .connecting:
+            if let url = URL(string: URLConstants.deepLinkDisconnectUrl) {
+                viewController?.extensionOpenUrl(url)
+            }
+        default:
+            if let url = URL(string: URLConstants.deepLinkConnectUrl) {
+                viewController?.extensionOpenUrl(url)
+            }
+        }
     }
 }
 
