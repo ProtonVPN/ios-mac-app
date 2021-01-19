66954b0dd3477f2a34202d31339d43620ecf48bd
Jaroslav Ozevič <jaroslav@protonmail.com>
2021-01-19 16:49:07 +0200

    Fixed print test coverage script

--- apps/macos/fastlane/print-coverage.sh
+++ apps/macos/fastlane/print-coverage.sh
@@ -2,5 +2,5 @@
 
 # Parses coverage file and prints out overall code coverage
 
-TOTAL_XCTEST_COVERAGE=`xcrun xccov view $1 | grep '.app' | head -1 | perl -pe 's/.+?(\d+\.\d+%).+/\1/'`
+TOTAL_XCTEST_COVERAGE=`xcrun xccov view --report $1 | grep '.app' | head -1 | perl -pe 's/.+?(\d+\.\d+%).+/\1/'`
 echo "Total test coverage: $TOTAL_XCTEST_COVERAGE"
