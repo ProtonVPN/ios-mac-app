40203c062a5e4b3fbe13dd9b0d11d46cd7eae6f5
Igor Kulman <igorkulman@protonmail.com>
2021-02-11 16:48:25 +0100

    Custom toggle view that does not immediately change state, just fires a callback [VPNAPPL-398]

---
+++ apps/ios/ProtonVPN/ConfirmationToggleSwitch.swift
@@ -0,0 +1,57 @@
+//
+//  ConfirmationToggleSwitch.swift
+//  ProtonVPN - Created on 11.02.2021.
+//
+//  Copyright (c) 2019 Proton Technologies AG
+//
+//  This file is part of ProtonVPN.
+//
+//  ProtonVPN is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ProtonVPN is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+import UIKit
+
+final class ConfirmationToggleSwitch: UISwitch {
+    var tapped: (() -> Void)?
+
+    private let button = UIButton()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    private func setup() {
+        addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.leadingAnchor.constraint(equalTo: leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: trailingAnchor),
+            button.topAnchor.constraint(equalTo: topAnchor),
+            button.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+
+        button.addTarget(self, action: #selector(switchTapped(sender:)), for: .touchUpInside)
+    }
+
+    @objc private func switchTapped(sender: UIButton) {
+        tapped?()
+    }
+}
