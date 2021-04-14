1a576bf8f762411a19b011085eeef97445d7024f
Igor Kulman <igorkulman@protonmail.com>
2021-04-14 12:17:09 +0200

    Dealing with VPN auth certificate fetching and storing [VPNAPPL-619]

---
+++ libraries/vpncore/Source/API/Requests/VpnAuth/CertificateRequest.swift
@@ -0,0 +1,62 @@
+//
+//  CertificateRequest.swift
+//  vpncore - Created on 06.04.2021.
+//
+//  Copyright (c) 2019 Proton Technologies AG
+//
+//  This file is part of vpncore.
+//
+//  vpncore is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  vpncore is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with vpncore.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Alamofire
+import Foundation
+
+final class CertificateRequest: BaseRequest {
+
+    let publicKey: String
+    let deviceName: String
+
+    init(publicKey: String) {
+        self.publicKey = publicKey
+        #if os(iOS)
+        deviceName = UIDevice.current.name
+        #else
+        deviceName = Host.current().localizedName ?? ""
+        #endif
+    }
+
+    // MARK: - Override
+
+    override func path() -> String {
+        return super.path() + "/vpn/v1/certificate"
+    }
+
+    override var method: HTTPMethod {
+        return .post
+    }
+
+    override var header: [String: String]? {
+        return authenticatedHeader
+    }
+
+    override var parameters: [String: Any]? {
+        return [
+            "ClientPublicKey": publicKey,
+            "ClientPublicKeyMode": "EC",
+            "DeviceName": deviceName,
+            "Mode": "session"
+        ]
+    }
+}
