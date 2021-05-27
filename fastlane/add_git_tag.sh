23e5f15862f281aa280fa2d43fc2ad168d6d0785
Jaroslav Ozevič <jaroslav@protonmail.com>
2021-05-27 16:49:58 +0300

    Push to git via https

--- fastlane/add_git_tag.sh
+++ fastlane/add_git_tag.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 
-# ios/beta/2.4.1-2105271204
-
 APP_OS="ios"
 VERSION_NUMBER=`grep -E "IOS_APP_VERSION" apps/ios/Config.xcconfig | grep -Eo "([0-9]+\.[0-9]+\.[0-9]+)"`
 BUILD_NUMBER=`cat .last_build_nr`
 GIT_TAG_NAME="$APP_OS/beta/$VERSION_NUMBER-$BUILD_NUMBER"
 echo $GIT_TAG_NAME
 
+git remote set-url origin "https://${GIT_CI_USERNAME}:${PRIVATE_TOKEN_GITLAB_API_PROTON_CI}@$(awk -F '@' '{print $2}' <<< "$CI_REPOSITORY_URL")";
 git tag $GIT_TAG_NAME
 git push origin $GIT_TAG_NAME
