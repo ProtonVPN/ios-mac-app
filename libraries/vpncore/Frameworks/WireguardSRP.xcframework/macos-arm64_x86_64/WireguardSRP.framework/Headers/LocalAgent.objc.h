076a6b389d690c281ac36af5faf37edfeba7091e
Igor Kulman <igorkulman@protonmail.com>
2021-06-15 12:11:33 +0200

    Shared library updated

--- libraries/vpncore/Frameworks/WireguardSRP.xcframework/macos-arm64_x86_64/WireguardSRP.framework/Headers/LocalAgent.objc.h
+++ libraries/vpncore/Frameworks/WireguardSRP.xcframework/macos-arm64_x86_64/WireguardSRP.framework/Headers/LocalAgent.objc.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74013fd63b116ed97c5606cd9573cf1408b9a7fe5c3cbb1dad758787d236639b
-size 7455
+oid sha256:b35b87c9c1d648e80114af1168a114c1697863a9b4f95ff711cc064db60be40d
+size 7496
