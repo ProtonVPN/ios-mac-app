3f37c66c1556e5f109f7e3d5764569e87a7615b5
Igor Kulman <igorkulman@protonmail.com>
2021-07-01 15:53:09 +0200

    Fix for WireGuard configuration not loaded in Widget correctly [VPNAPPL-755]

--- apps/ios/Quick Connect Widget/WidgetFactory.swift
+++ apps/ios/Quick Connect Widget/WidgetFactory.swift
@@ -25,7 +25,7 @@ import vpncore
 
 final class WidgetFactory {
     private let openVpnExtensionBundleIdentifier = "ch.protonmail.vpn.OpenVPN-Extension"
-    private let wireguardVpnExtensionBundleIdentifier = "ch.protonmail.vpn.WireGuard-Extension"
+    private let wireguardVpnExtensionBundleIdentifier = "ch.protonmail.vpn.WireGuardiOS-Extension"
     private let appGroup = AppConstants.AppGroups.main
 
     private let alertService = ExtensionAlertService()
