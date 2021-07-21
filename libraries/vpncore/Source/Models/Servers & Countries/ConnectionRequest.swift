dcdfe95cfd8eb848719f91c4f3b95596c6532b9d
Igor Kulman <igorkulman@protonmail.com>
2021-07-21 17:36:42 +0200

    Fix for connection request not being created correctly for a protocol change [VPNAPPL-825]

--- libraries/vpncore/Source/Models/Servers & Countries/ConnectionRequest.swift
+++ libraries/vpncore/Source/Models/Servers & Countries/ConnectionRequest.swift
@@ -70,7 +70,7 @@ public struct ConnectionRequest: Codable {
     }
     
     public func withChanged(vpnProtocol: VpnProtocol) -> ConnectionRequest {
-        return ConnectionRequest(serverType: self.serverType, connectionType: self.connectionType, connectionProtocol: self.connectionProtocol, netShieldType: self.netShieldType, profileId: self.profileId)
+        return ConnectionRequest(serverType: self.serverType, connectionType: self.connectionType, connectionProtocol: .vpnProtocol(vpnProtocol), netShieldType: self.netShieldType, profileId: self.profileId)
     }
 
     private enum Keys: CodingKey {
