0bc6e8a08ac1292e0a7a9cfa84277cb38eaaf1ea
Igor Kulman <igor.kulman@proton.ch>
2021-07-30 09:47:29 +0200

    Using correct server name reconnecting local agent after cold start [VPNAPPL-859]

--- libraries/vpncore/Source/Models/LocalAgent/LocalAgentConfiguration.swift
+++ libraries/vpncore/Source/Models/LocalAgent/LocalAgentConfiguration.swift
@@ -44,6 +44,6 @@ extension LocalAgentConfiguration {
             return nil
         }
 
-        self.init(hostname: connectionConfiguration.server.domain, netshield: propertiesManager.netShieldType ?? .off, vpnAccelerator: !propertiesManager.featureFlags.isVpnAccelerator || propertiesManager.vpnAcceleratorEnabled, bouncing: connectionConfiguration.serverIp.label)
+        self.init(hostname: connectionConfiguration.serverIp.domain, netshield: propertiesManager.netShieldType ?? .off, vpnAccelerator: !propertiesManager.featureFlags.isVpnAccelerator || propertiesManager.vpnAcceleratorEnabled, bouncing: connectionConfiguration.serverIp.label)
     }
 }
