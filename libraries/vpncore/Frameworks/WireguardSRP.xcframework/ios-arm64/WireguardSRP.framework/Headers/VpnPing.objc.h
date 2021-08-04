73a519ffdd9013c1cbd656495907fe2dd71bf312
Igor Kulman <igor.kulman@proton.ch>
2021-08-04 15:09:48 +0200

    Using latest shared library with ping timeout in miliseconds [VPNAPPL-868]

--- libraries/vpncore/Frameworks/WireguardSRP.xcframework/ios-arm64/WireguardSRP.framework/Headers/VpnPing.objc.h
+++ libraries/vpncore/Frameworks/WireguardSRP.xcframework/ios-arm64/WireguardSRP.framework/Headers/VpnPing.objc.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7af611943a93505a3f13023c7ec5e6a094b7b83a6358549a91a332a0134e43f
-size 538
+oid sha256:cc7473756dae7a963be9ca17d3a0701c44e3eb03488046434f694b00e5a3d508
+size 543
