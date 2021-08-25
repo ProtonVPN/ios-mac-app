49be7bf2a797afd919cb4e6f86e28e634125db37
Igor Kulman <igor.kulman@proton.ch>
2021-08-25 15:57:42 +0200

    Adding missing protocol conformance in mocks

--- libraries/vpncore/Tests/Mocks/NetworkingMock.swift
+++ libraries/vpncore/Tests/Mocks/NetworkingMock.swift
@@ -8,8 +8,14 @@
 
 import Foundation
 import ProtonCore_Networking
+import ProtonCore_Services
+import ProtonCore_Authentication
 
 final class NetworkingMock: Networking {
+    func request(_ route: LoginRequest, completion: @escaping (Result<Authenticator.Status, AuthErrors>) -> Void) {
+
+    }
+
     func request(_ route: Request, completion: @escaping (Result<JSONDictionary, Error>) -> Void) {
 
     }
@@ -21,4 +27,23 @@ final class NetworkingMock: Networking {
     func request(_ route: URLRequest, completion: @escaping (Result<String, Error>) -> Void) {
 
     }
+}
+
+extension NetworkingMock: APIServiceDelegate {
+    public var locale: String {
+        return NSLocale.current.languageCode ?? "en_US"
+    }
+    public var appVersion: String {
+        return ApiConstants.appVersion
+    }
+    public var userAgent: String? {
+        return ApiConstants.userAgent
+    }
+    public func onUpdate(serverTime: Int64) {
+
+    }
+    public func isReachable() -> Bool {
+        return true
+    }
+    public func onDohTroubleshot() { }
 }
