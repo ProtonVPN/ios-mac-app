71d3f94ad631f63ee536d16a76a8c5d2e6e2b7d4
Igor Kulman <igor.kulman@proton.ch>
2021-10-05 15:19:20 +0200

    Extending the announcement data model [VPNAPPL-770] [VPNAPPL-962]

--- libraries/vpncore/Source/Models/Core/Offer.swift
+++ libraries/vpncore/Source/Models/Core/Offer.swift
@@ -23,16 +23,42 @@
 import Foundation
 
 public struct Offer: Codable {
-    
     public let label: String
     public let url: String
     public let icon: String
+    public let panel: OfferPanel?
     
     // Our decoding strategy changes first letter to lowercase
     enum CodingKeys: String, CodingKey {
-            case label = "label"
-            case url = "uRL"
-            case icon = "icon"
-        }
-    
+        case label
+        case url = "uRL"
+        case icon
+        case panel
+    }
+}
+
+public struct OfferPanel: Codable {
+    public let incentive: String
+    public let incentivePrice: String
+    public let pill: String
+    public let pictureURL: String
+    public let title: String
+    public let features: [OfferFeature]
+    public let featuresFooter: String
+    public let pageFooter: String
+}
+
+public struct OfferFeature: Codable {
+    public let iconURL: String
+    public let text: String
+}
+
+public struct OfferButton: Codable {
+    public let url: String
+    public let text: String
+
+    enum CodingKeys: String, CodingKey {
+        case text
+        case url = "uRL"
+    }
 }
