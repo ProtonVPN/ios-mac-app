197ef1ff362f8d272fb8d699bf865f912868f622
Igor Kulman <igor.kulman@proton.ch>
2021-11-29 09:11:44 +0100

    Adding a way to send unauthenticated bug report request

---
+++ libraries/vpncore/Source/API/Requests/Core/ReportsBugs.swift
@@ -0,0 +1,59 @@
+//
+//  Created on 29.11.2021.
+//
+//  Copyright (c) 2021 Proton AG
+//
+//  ProtonVPN is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ProtonVPN is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+
+import Foundation
+import ProtonCore_APIClient
+import ProtonCore_Networking
+
+public final class ReportsBugs: Request {
+
+    public let bug: ReportBug
+
+    public init( _ bug: ReportBug) {
+        self.bug = bug
+    }
+
+    public var path: String {
+        return "/reports/bug"
+    }
+
+    public var method: HTTPMethod {
+        return .post
+    }
+
+    public var parameters: [String: Any]? {
+        return [
+            "OS": bug.os,
+            "OSVersion": bug.osVersion,
+            "Client": bug.client,
+            "ClientVersion": bug.clientVersion,
+            "ClientType": String(bug.clientType),
+            "Title": bug.title,
+            "Description": bug.description,
+            "Username": bug.username,
+            "Email": bug.email,
+            "Country": bug.country,
+            "ISP": bug.ISP,
+            "Plan": bug.plan
+        ]
+    }
+
+    public var isAuth: Bool {
+        return AuthKeychain.fetch() != nil
+    }
+}
