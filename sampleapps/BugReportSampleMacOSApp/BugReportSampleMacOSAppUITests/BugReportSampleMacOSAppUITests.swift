e3148ccde630d349f9778cb6212ff7572686e63c
Jaroslav Ozevič <jaroslav@protonmail.com>
2022-02-01 14:06:35 +0200

    Better example test names [VPNAPPL-1001], [VPNAPPL-1002]

--- sampleapps/BugReportSampleMacOSApp/BugReportSampleMacOSAppUITests/BugReportSampleMacOSAppUITests.swift
+++ sampleapps/BugReportSampleMacOSApp/BugReportSampleMacOSAppUITests/BugReportSampleMacOSAppUITests.swift
@@ -33,7 +33,7 @@ class BugReportSampleMacOSAppUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testFormIsOpened() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
--- sampleapps/BugReportSampleiOSApp/BugReportSampleiOSAppUITests/BugReportSampleiOSAppUITests.swift
+++ sampleapps/BugReportSampleiOSApp/BugReportSampleiOSAppUITests/BugReportSampleiOSAppUITests.swift
@@ -33,7 +33,7 @@ class BugReportSampleiOSAppUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testFilledInFormCanBeSent() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launchArguments = ["UITests"]
