6da6ea4363e1825b37e76d958049cf0636eda8d9
Igor Kulman <igor.kulman@proton.ch>
2022-02-04 13:47:51 +0100

    Script for generating proper Xcode projects for local SPM packages because string export does not work for SPM [VPNAPPL-1090]

---
+++ fastlane/adjust-for-strings-export.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script generates full Xcode projects for all the SPM packages as their are needed for strings export
+# String export is done in the CI using `xcodebuild -exportLocalizations -localizationPath $outputDir -exportLanguage en`
+
+# make sure dependencies are installed
+xcodegen --version || brew install xcodegen
+
+for package in libraries/**/Package.swift
+do
+    echo $package
+    name=($(basename "$(dirname "$package")"))
+    path="libraries/$name"
+
+    # create a project definition file with just the strings
+    cat << EOF > "$path/project.yml"
+name: $name
+targets:
+  $name:
+    type: framework
+    platform: iOS
+    deploymentTarget: "12.0"
+    sources: [Sources/$name/Resources]
+EOF
+    # generate a classic Xcode project from the definition file
+    # not doing it via `swift package generate-xcodeproj` because it is deprecated and ignores resource files mostly
+    cd $path
+    xcodegen generate
+    cd -    
+done
