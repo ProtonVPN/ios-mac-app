34268fcaea6e518ef03eb1aed49d083c32ff2937
Jaroslav Ozevič <jaroslav@protonmail.com>
2022-02-14 09:49:58 +0200

    Fixed BugReport translations and assets [VPNAPPL-1001] [VPNAPPL-1002]

--- libraries/BugReport/Package.swift
+++ libraries/BugReport/Package.swift
@@ -10,19 +10,18 @@ let package = Package(
         .iOS(.v12), // On iOS < 14, Creator will return nil, so the app can use old, compatible BugReport.
         .macOS(.v10_15)], // On macOS < 11, Creator will return nil, so the app can use old, compatible BugReport.
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BugReport",
             targets: ["BugReport"]),
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BugReport",
             dependencies: [],
-            resources: []
+            resources: [
+                .process("Resources")
+            ]
         ),
         .testTarget(
             name: "BugReportTests",
