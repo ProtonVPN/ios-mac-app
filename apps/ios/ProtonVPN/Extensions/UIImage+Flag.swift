aa781c9d7c18d23fe075b42eaed8c0c0b57f02fd
Igor Kulman <igor.kulman@proton.ch>
2022-03-23 13:42:04 +0100

    Correctly loading the UK flag from Core  [VPNAPPL-1198]

--- apps/ios/ProtonVPN/Extensions/UIImage+Flag.swift
+++ apps/ios/ProtonVPN/Extensions/UIImage+Flag.swift
@@ -22,6 +22,9 @@ import ProtonCore_UIFoundations
 
 extension UIImage {
     static func flag(countryCode: String) -> UIImage? {
+        // Core uses GB instead of UK for The United Kingdom
+        let countryCode = countryCode == "UK" ? "GB" : countryCode
+
         // normaly we would use IconProvider.flag(forCountryCode: countryCode) but it crashes with country codes that do not have a valid icon
         guard let url = Bundle(for: PMUIFoundations.self).resourceURL?.appendingPathComponent("Resources-UIFoundations.bundle"), let bundle = Bundle(url: url) else {
             return nil
