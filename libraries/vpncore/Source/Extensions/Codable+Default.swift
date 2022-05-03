69d196ba0fcdabf158680c19be403af46c23d77e
Igor Kulman <igor.kulman@proton.ch>
2022-05-03 16:55:26 +0200

    Adding support for using default boolean values in JSON decoding

---
+++ libraries/vpncore/Source/Extensions/Codable+Default.swift
@@ -0,0 +1,57 @@
+//
+//  Created on 03.05.2022.
+//
+//  Copyright (c) 2022 Proton AG
+//
+//  ProtonVPN is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ProtonVPN is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+
+import Foundation
+
+public protocol DefaultValue {
+    associatedtype Value: Codable
+    static var defaultValue: Value { get }
+}
+
+extension Bool: DefaultValue {
+    public static let defaultValue = false
+}
+
+@propertyWrapper
+public struct Default<T: DefaultValue> {
+    public var wrappedValue: T.Value
+
+    public init(wrappedValue: T.Value) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+extension Default: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        wrappedValue = (try? container.decode(T.Value.self)) ?? T.defaultValue
+    }
+}
+
+extension Default: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
+    }
+}
+
+public extension KeyedDecodingContainer {
+    func decode<T>(_ type: Default<T>.Type, forKey key: Key) throws -> Default<T> where T: DefaultValue {
+        try decodeIfPresent(type, forKey: key) ?? Default(wrappedValue: T.defaultValue)
+    }
+}
