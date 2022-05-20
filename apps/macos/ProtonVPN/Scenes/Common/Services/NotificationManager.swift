525033cf2d2b8ea100d023e8c32b36ce6d76cba7
John Biggs <john.biggs@proton.ch>
2022-05-20 14:05:01 +0200

    Revert "Rebrand system notification icons [VPNAPPL-1302]"
    This reverts commit d9e69da9c0ee9677476374fd87d20b60161221aa.

--- apps/macos/ProtonVPN/Scenes/Common/Services/NotificationManager.swift
+++ apps/macos/ProtonVPN/Scenes/Common/Services/NotificationManager.swift
@@ -68,7 +68,6 @@ class NotificationManager: NSObject, NotificationManagerProtocol {
     
     private func connectedNotification(for server: ServerModel) -> NSUserNotification {
         let notification = NSUserNotification()
-        notification.contentImage = AppTheme.Icon.appIconConnected
         notification.title = "Proton VPN " + LocalizedString.connected
         notification.subtitle = connectSubtitle(forServer: server)
         notification.informativeText = connectInformativeText(forServer: server)
@@ -107,7 +106,6 @@ extension NotificationManager: NSUserNotificationCenterDelegate {
 extension NotificationManager {
     func displayServerGoingOnMaintenance() {
         let notification = NSUserNotification()
-        notification.contentImage = AppTheme.Icon.appIconDisconnected
         notification.title = LocalizedString.maintenanceOnServerDetectedTitle
         notification.subtitle = LocalizedString.maintenanceOnServerDetectedSubtitle
         notification.informativeText = LocalizedString.maintenanceOnServerDetectedSubtitle
