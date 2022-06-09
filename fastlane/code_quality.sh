aa48d2effc30827f5a517719e89b4643ce45b976
Igor Kulman <igor.kulman@proton.ch>
2022-06-09 14:15:28 +0200

    Adding code quality stage using SwiftLint [VPNAPPL-1314]

---
+++ fastlane/code_quality.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function process() {
+    name=($(basename "$(dirname "$2")"))
+    path="$1/$name"
+    
+    cd $path
+    ../../Pods/SwiftLint/swiftlint --strict --reporter codeclimate > codequality_report.json
+
+    pattern="\"path\" : \""
+    replacement="\"path\" : \"$1\/$name\/"
+    sed -i'' -e "s/$pattern/$replacement/g" codequality_report.json
+    cd -
+}
+
+for target in libraries/**/.swiftlint.yml
+do
+    process "libraries" $target
+done
+
+jq -s '[.[][]]' libraries/**/codequality_report.json > codequality_report.json
+
+for target in apps/**/.swiftlint.yml
+do
+    process "apps" $target
+done
+
+jq -s '[.[][]]' apps/**/codequality_report.json > codequality_report.json
