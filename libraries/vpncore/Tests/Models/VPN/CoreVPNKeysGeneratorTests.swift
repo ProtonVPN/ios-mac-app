ecc117efdc704061647a29b834b527874d8e1071
Jaroslav Ozevič <jaroslav@protonmail.com>
2022-10-19 16:34:07 +0300

    Added CoreVPNKeysGeneratorTests [VPNAPPL-1394]

---
+++ libraries/vpncore/Tests/Models/VPN/CoreVPNKeysGeneratorTests.swift
@@ -0,0 +1,37 @@
+//
+//  Created on 2022-10-19.
+//
+//  Copyright (c) 2022 Proton AG
+//
+//  ProtonVPN is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ProtonVPN is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+
+import XCTest
+@testable import vpncore
+
+final class CoreVPNKeysGeneratorTests: XCTestCase {
+
+    func testDifferentKeysAreGenerated() throws {
+        let generator = CoreVPNKeysGenerator()
+        let key1 = generator.generateKeys()
+        XCTAssertFalse(key1.publicKey.derRepresentation.isEmpty)
+        XCTAssertFalse(key1.privateKey.derRepresentation.isEmpty)
+        
+        let key2 = generator.generateKeys()
+        XCTAssertFalse(key2.publicKey.derRepresentation.isEmpty)
+        XCTAssertFalse(key2.privateKey.derRepresentation.isEmpty)
+        
+        XCTAssertNotEqual(key1.publicKey.derRepresentation, key2.publicKey.derRepresentation)
+    }
+
+}
