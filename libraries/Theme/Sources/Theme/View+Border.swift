3ffafc19f5c45b25896baf7ed7c246fb66865a23
Pawel Jurczyk <pawel.jurczyk@proton.ch>
2023-04-20 14:03:53 +0200

    feature(ui): Add border to Theme
    Jira-Id: VPNAPPL-1703

---
+++ libraries/Theme/Sources/Theme/View+Border.swift
@@ -0,0 +1,30 @@
+//
+//  Created on 20/04/2023.
+//
+//  Copyright (c) 2023 Proton AG
+//
+//  ProtonVPN is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ProtonVPN is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+
+import SwiftUI
+
+extension View {
+    public func themeBorder(color: Color,
+                            lineWidth: CGFloat,
+                            cornerRadius: AppTheme.CornerRadius) -> some View {
+        return self.overlay(
+            RoundedRectangle(cornerRadius: cornerRadius.rawValue)
+                .stroke(color, lineWidth: lineWidth)
+        )
+    }
+}
